8c73ed7302191619bcae46dfe462e8dbe731f9eb
Alexandre MONTORO <alexandre.montoro@viacesi.fr>
2019-07-01 09:21:57 +0200

    Add JsonProperty

--- Altantis_DeviceSensorEP/Business/RawData.cs
+++ Altantis_DeviceSensorEP/Business/RawData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,17 +8,22 @@ namespace Altantis_DeviceSensorEP.Business
 {
     public class RawData
     {
+        [JsonProperty(PropertyName = "macAddress")]
         public string MacAddress { get; set; }
+        [JsonProperty(PropertyName = "metricDate")]
         public DateTime MetricDate { get; set; }
-        public string SensorType { get; set; }
+        [JsonProperty(PropertyName = "deviceType")]
+        public string DeviceType { get; set; }
+        [JsonProperty(PropertyName = "name")]
         public string Name { get; set; }
+        [JsonProperty(PropertyName = "metricValue")]
         public double MetricValue { get; set; }
 
-        public RawData(string macAddress, DateTime metricDate, string sensorType, string name, double metricValue)
+        public RawData(string macAddress, DateTime metricDate, string deviceType, string name, double metricValue)
         {
             MacAddress = macAddress;
             MetricDate = metricDate;
-            SensorType = sensorType;
+            DeviceType = deviceType;
             Name = name;
             MetricValue = metricValue;
         }
